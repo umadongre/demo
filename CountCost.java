faa9e63c8e708b9f3772c77896db68cdaa4382bc
umadongre <umadongre92@gmail.com>
2020-05-02 14:57:07 +0530

    Test Demo

---
+++ CountCost.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class CountCost {
+   static int min;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] costs= {600,800,150,700};
+       int totalminCost = 0;
+       boolean flag=false;
+       ArrayList<Integer> reachedtemple=new ArrayList<Integer>();
+       ArrayList<Integer> costList=new ArrayList<Integer>();
+         for (Integer cost : costs) {
+			costList.add(cost);
+		}
+         Collections.sort(costList);
+         
+         System.out.println(costList.size());
+         if(costList.size() ==2) {
+        	 min=costList.get(1);
+         }else {
+         
+         for(int ind=0;ind<=costList.size()-1;ind++) {
+        	 flag=false;
+        	 reachedtemple.add(costList.get(ind));
+        	 if((ind+1)<costList.size()) {
+        	 reachedtemple.add(costList.get(ind+1));
+        	 totalminCost=totalminCost+costList.get(ind+1);
+        	 ind++;
+        	 flag=true;
+        	 }
+        	 if(flag==true) {
+        	 min=findmin(reachedtemple);
+        	 reachedtemple.remove(0);
+        	 min=totalminCost+min;
+        	 }else {
+        		 min=min+costList.get(ind);
+        	 }
+        	
+         }
+         }
+         System.out.println("Cost required to reach village"+min);
+	}
+	private static int findmin(ArrayList<Integer> reachedtemple) {
+		// TODO Auto-generated method stub
+		 Collections.sort(reachedtemple);
+		return min+reachedtemple.get(0);
+	}
+	
+
+}
